47fa08a5d4231081e625d3ae7afd05b6bb0254fc
Hashem Almubarak <105522749+Hashim11Mub@users.noreply.github.com>
2023-12-19 01:11:41 +0300

    large changes

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const userRouter = express.Router;
+const userRouter = express.Router();
 
-userRouter.get('/', getAllUsers);
-userRouter.post('/sginup', sifnUp);
-userRouter.put(':id', updateUser);
-userRouter.delete('/:id', deleteUser);
-userRouter.post('/login', logIn);
-userRouter.get('/bookings/:id', getBookingofUser);
-userRouter.get('/:id', getUserById);
+const { getAllUser, signUp, updateUser, deleteUser, logIn, getBookingofUser, getUserById } = require('../controllers/user-controller');
 
-module.exports = userRouter;
+userRouter.get("/", getAllUser);
+userRouter.post("/signup", signUp)
+userRouter.put("/:id", updateUser)
+userRouter.delete("/:id", deleteUser)
+userRouter.post("/login", logIn)
+userRouter.get("/bookings/:id", getBookingofUser);
+userRouter.get("/:id", getUserById);
+
+module.exports = userRouter 
